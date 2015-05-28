ecc9a71ac5b393c71778e06b67ebf81845e3eab5
Julia Van Cleve <julia@jmv.me>
2015-05-27 21:36:15 -0700

    search for followers

---
+++ searchfilter.js
@@ -0,0 +1,48 @@
+// searchable list courtesy of Rob Sawyer
+http://www.robsawyer.me/blog/2013/07/10/lightweight-jquery-searchfilter-tutorial/
+
+$(document).ready(function() {
+
+  //we want this function to fire whenever the user types in the search-box
+  $("#search").keyup(function () {
+  
+    //first we create a variable for the value from the search-box
+    var searchTerm = $("#search").val();
+
+    //then a variable for the list-items (to keep things clean)
+    var listItem = $('#search_list').children('li');
+    
+    //extends the default :contains functionality to be case insensitive
+    //if you want case sensitive search, just remove this next chunk
+    $.extend($.expr[':'], {
+      'containsi': function(elem, i, match, array)
+      {
+        return (elem.textContent || elem.innerText || '').toLowerCase()
+        .indexOf((match[3] || "").toLowerCase()) >= 0;
+      }
+    });//end of case insensitive chunk
+
+
+    //this part is optional
+    //here we are replacing the spaces with another :contains
+    //what this does is to make the search less exact by searching all words and not full strings
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+    
+    //here is the meat. We are searching the list based on the search terms
+    $("#masterlist #search_list li.followers").not(":containsi('" + searchSplit + "')").each(function(e)   {
+
+          //add a "hidden" class that will remove the item from the list
+          $(this).addClass('hidden');
+
+    });
+    
+    //this does the opposite -- brings items back into view
+    $("#masterlist #search_list li:containsi('" + searchSplit + "')").each(function(e) {
+
+          //remove the hidden class (reintroduce the item to the list)
+          $(this).removeClass('hidden');
+
+    });
+  });
+});
---
+++ skyrim.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    count = 0;
+    $('#compare').addClass("sortable");
+    $(".sortable").sortable({
+        items: "li:not(.row-title)",
+        axis: "x",
+        revert: true,
+        scroll: false,
+        placeholder: "sortable-placeholder",
+    });
+    $("li.followers").click(function(){
+        if ( !$(this).hasClass("selected") ) {
+            $(this).addClass(count.toString());
+            $('#compare').append( $(this).clone() );
+            $(this).addClass("selected");
+            count = count + 1;
+        }
+        else {
+            id = $(this).attr("class").split(' ');
+            $(this).removeClass("selected");
+            $(this).removeClass(id[1]);
+            if ($('#compare > li').hasClass(id[1])) {
+                classid = "." + id[1];
+                $(classid).remove();
+            }
+        }
+    });
+    $("button").click(function(){
+        $('#compare > li.followers').remove();
+        $('li.followers').removeClass();
+        $('#masterlist ul > li').addClass("followers")
+        count = 0;
+    });
+});
